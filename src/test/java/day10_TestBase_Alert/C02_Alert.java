2b339bff7d853c9003b0802c536b3a37d6bcde5d
mustafa <aslanbird940@gmail.com>
2023-06-12 23:50:05 +0300

    12/06/2023

---
+++ src/test/java/day10_TestBase_Alert/C02_Alert.java
@@ -0,0 +1,93 @@
+package day10_TestBase_Alert;
+
+import org.junit.Assert;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.TestBase;
+
+public class C02_Alert extends TestBase {
+
+        /*
+    Eğer bir sayfadaki bir buttona tıkladığımızda bir uyarı penceresi çıkıyorsa ve bu çıkan pencereye sağ klip
+ yapıp locate alamıyorsak, bu bir js Alert'tür.
+    js Alert'ü handle edebilmek için driver'ımızı o pencere geçirmemiz gerekir. Bunun için;
+ driver objemizi kullanarak switchTo() methoduyla alert() methodunu kullanarak js alert'e geçiş yapmış oluruz
+ accept() yada dismiss() methodlarıyla js Alert'ü onaylar yada iptal ederek kapatırız
+     */
+
+        //https://testcenter.techproeducation.com/index.php?page=javascript-alerts adresine gidin.
+        //Bir metod olusturun: acceptAlert
+        //1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının  “You successfully clicked an alert” oldugunu test edin.
+        //Bir metod olusturun: dismissAlert
+        //2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının
+        //“successfuly” icermedigini test edin.
+        //Bir metod olusturun: sendKeysAlert
+        //3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna
+        //tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
+
+    @Test
+    public void  acceptAlert()  {
+        //Bir metod olusturun: acceptAlert
+//https://testcenter.techproeducation.com/index.php?page=javascript-alerts adresine gidin.
+        driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
+
+//1. butona tıklayın, uyarıdaki OK butonuna tıklayın ve result mesajının "You successfully clicked an alert" oldugunu test edin.
+        driver.findElement(By.xpath("(//button)[1]")).click();
+        bekle(3);
+        driver.switchTo().//geçmek için kullanılan method
+                alert().//alert'e geçiş yapar
+                accept();//çıkan alert'te ok yada tamam butonuna tıklar
+        bekle(2);
+        WebElement resultMessage = driver.findElement(By.xpath("(//p)[2]"));
+        String istenenYazi = "You successfully clicked an alert";
+        Assert.assertEquals(istenenYazi,resultMessage.getText());
+    }
+
+    @Test
+    public void dismissAlert() {
+        //Bir metod olusturun: dismissAlert
+        //https://testcenter.techproeducation.com/index.php?page=javascript-alerts adresine gidin.
+
+        driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
+
+        //2. butona tıklayın, uyarıdaki Cancel butonuna tıklayın ve result mesajının “successfuly” icermedigini test edin.
+
+        driver.findElement(By.xpath("(//button)[2]")).click();
+        bekle(3);
+        driver.switchTo().
+                alert().
+                dismiss();//js Alert'teki iptal butonuna basar
+
+        WebElement resultMessage = driver.findElement(By.xpath("(//p)[2]"));
+        String istenenMetin = "successfuly";
+        Assert.assertFalse(resultMessage.getText().contains(istenenMetin));
+
+    }
+    @Test @Ignore //--> Çalışmasını istemediğimiz test notasyonundan sonra (@Test) @Ignore notasyonu kullanırız
+    public void senKeysAlert() {
+        //Bir metod olusturun: sendKeysAlert
+        //https://testcenter.techproeducation.com/index.php?page=javascript-alerts adresine gidin.
+        driver.get("https://testcenter.techproeducation.com/index.php?page=javascript-alerts");
+
+        //3. butona tıklayın, uyarıdaki metin kutusuna isminizi yazin, OK butonuna
+        //tıklayın ve result mesajında isminizin görüntülendiğini doğrulayın.
+
+        driver.findElement(By.xpath("(//button)[3]")).click();
+        bekle(3);
+        driver.switchTo().alert().sendKeys("Erol"+Keys.ENTER);
+        bekle(3);
+        driver.switchTo().alert().accept();
+        bekle(3);
+
+        WebElement resultMessage = driver.findElement(By.xpath("(//p)[2]"));//You entered: erol
+        String girileneMetin = "Erol";
+        Assert.assertTrue(resultMessage.getText().contains(girileneMetin));
+        bekle(3);
+
+
+
+    }
+}
