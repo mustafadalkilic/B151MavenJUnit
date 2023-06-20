58639b353dd3787514e71593a90b684ba20c543e
mustafa <aslanbird940@gmail.com>
2023-06-20 23:36:51 +0300

    20/06/2023

---
+++ src/test/java/day16_SeleniumWait/C04_ExplicitWaitClassWork.java
@@ -0,0 +1,31 @@
+package day16_SeleniumWait;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import utilities.TestBase;
+
+import java.time.Duration;
+
+public class C04_ExplicitWaitClassWork extends TestBase {
+    @Test
+    public void test01() {
+
+        //https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver adresine gidelim
+        driver.get("https://chercher.tech/practice/explicit-wait-sample-selenium-webdriver");
+
+        //Click me to open an Alert buttonuna basalım
+        driver.findElement(By.xpath("(//button)[3]")).click();
+        new WebDriverWait(driver, Duration.ofSeconds(20)).until(ExpectedConditions.alertIsPresent());
+        //Alert cıkana kadar bekleyecek
+
+        //Çıkan Alert'ü kapatalım
+        acceptAlert();
+
+
+
+
+    }
+}
